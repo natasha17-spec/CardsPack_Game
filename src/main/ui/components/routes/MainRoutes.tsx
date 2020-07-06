30348b0219f3ef42671132df13ccb5c2b7134dca
natasha17-spec <17061990@inbox.ru>
2020-07-06 23:40:34 +0300

    added files

--- src/main/ui/components/routes/MainRoutes.tsx
+++ src/main/ui/components/routes/MainRoutes.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import Root from '../root/Root';
 import Game from "../game/Game";
-import Create from "../create_cards/Create";
+import Create from "../create_cards/CreateContainer";
 import Find from "../find_deck/Find";
 import Home from "../home/Home";
 import Profile from '../profile/ui/Profile';
 import ProfileContainer from '../profile/ui/ProfileContainer';
+import FindContainer from '../find_deck/FindContainer';
 
 
 export const ROOT_PATH = '/';
@@ -25,7 +26,8 @@ const MainRoutes = () => {
             <Route exact path={PROFILE_PATH} render={() => <ProfileContainer/>}/>
             <Route path={GAME_PATH} render={() => <Game/>}/>
             <Route exact path={CREATE_CARDS_PATH} render={() => <Create/>}/>
-            <Route exact path={FIND_DECK_PATH} render={() => <Find/>}/>
+            {/*<Route exact path={FIND_DECK_PATH} render={() => <Find/>}/>*/}
+            <Route exact path={FIND_DECK_PATH} render={() => <FindContainer/>}/>
             <Route exact path={HOME_PATH} render={() => <Home/>}/>
         </>
     )
